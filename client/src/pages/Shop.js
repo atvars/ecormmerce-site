e24254963cecf258cafc8fef670b644084723baf
atvars <avelavs@gmail.com>
2022-05-24 14:00:20 +0100

    started shop page and adder new component typebar

--- client/src/pages/Shop.js
+++ client/src/pages/Shop.js
@@ -1,7 +1,17 @@
 import React from 'react';
+import { Container } from 'react-bootstrap';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 const Shop = () => {
-  return <div>Shop</div>;
+  return (
+    <Container>
+      <Row>
+        <Col md={3}></Col>
+        <Col md={9}></Col>
+      </Row>
+    </Container>
+  );
 };
 
 export default Shop;
