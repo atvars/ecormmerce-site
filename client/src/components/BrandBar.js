5f142f7b5a8c96c8b14b364c2495a904aa04b4db
atvars <avelavs@gmail.com>
2022-06-14 10:22:27 +0100

    added new component ad pictures

--- client/src/components/BrandBar.js
+++ client/src/components/BrandBar.js
@@ -1,5 +1,7 @@
 import { Context } from '../index';
 import { Card, Row } from 'react-bootstrap';
+import React, { useContext } from 'react';
+import { observer } from 'mobx-react-lite';
 
 const BrandBar = observer(() => {
   const { device } = useContext(Context);
@@ -7,10 +9,18 @@ const BrandBar = observer(() => {
   return (
     <Row className='d-flex'>
       {device.brands.map((brand) => (
-        <Card key={brand.id} className='p-3'>
+        <Card
+          style={{ cursor: 'pointer' }}
+          key={brand.id}
+          className='p-3'
+          onClick={() => device.setSelectedBrand(brand)}
+          border={brand.id === device.selectedBrand.id ? 'danger' : 'light'}
+        >
           {brand.name}
         </Card>
       ))}
     </Row>
   );
 });
+
+export default BrandBar;
--- client/src/pages/Shop.js
+++ client/src/pages/Shop.js
@@ -3,6 +3,8 @@ import { Container } from 'react-bootstrap';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import TypeBar from '../components/TypeBar';
+import BrandBar from '../components/BrandBar';
+import DeviceList from '../components/DeviceList';
 
 const Shop = () => {
   return (
@@ -11,7 +13,10 @@ const Shop = () => {
         <Col md={3}>
           <TypeBar />
         </Col>
-        <Col md={9}></Col>
+        <Col md={9}>
+          <BrandBar></BrandBar>
+          <DeviceList></DeviceList>
+        </Col>
       </Row>
     </Container>
   );
